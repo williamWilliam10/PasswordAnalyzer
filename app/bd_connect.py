79991ef0285f8f70a3b43fd5f4cd5f5c2aa64689
william lowe <lowewilliam237@gmail.com>
2024-10-19 14:49:09 +0200

    modification du  fichier de connexion a la bd pour le deploiement

--- app/bd_connect.py
+++ app/bd_connect.py
@@ -2,15 +2,33 @@ import mysql.connector
 from mysql.connector import Error
 import os  # Importation pour lire les variables d'environnement
 
+
 def get_db_connection():
     try:
+        # Récupérer les variables d'environnement
+        host = os.environ.get("MYSQL_HOST")  # Hôte MySQL
+        user = os.environ.get("MYSQL_USER")  # Utilisateur MySQL
+        password = os.environ.get("MYSQL_PASSWORD")  # Mot de passe MySQL
+        database = os.environ.get("MYSQL_DATABASE")  # Base de données MySQL
+        port = os.environ.get("MYSQL_PORT")  # Port MySQL
+
+        # Vérifier que toutes les variables d'environnement sont définies
+        if None in [host, user, password, database, port]:
+            print("Erreur: Une ou plusieurs variables d'environnement ne sont pas définies.")
+            return None
+
+        # Conversion du port en entier
+        port = int(port)
+
+        # Connexion à la base de données
         connection = mysql.connector.connect(
-            host=os.environ.get("mysql.railway.internal"),          # Hôte MySQL fourni par Railway
-            user=os.environ.get("root"),          # Utilisateur MySQL fourni par Railway
-            password=os.environ.get("koNVTUXkFZcbxYpQUfxPvyWQdZJyFYrA"),  # Mot de passe MySQL fourni par Railway
-            database=os.environ.get("railway"),  # Base de données MySQL
-            port=os.environ.get("3306")           # Port MySQL fourni par Railway
+            host=host,
+            user=user,
+            password=password,
+            database=database,
+            port=port
         )
+
         if connection.is_connected():
             return connection
     except Error as e:
