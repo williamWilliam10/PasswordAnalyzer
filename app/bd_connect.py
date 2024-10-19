5d30805de133a695a35215289ba2e62c1c701326
william lowe <lowewilliam237@gmail.com>
2024-10-19 14:58:23 +0200

    utilisation des variable d'environement pour la bd

--- app/bd_connect.py
+++ app/bd_connect.py
@@ -1,36 +1,19 @@
 import mysql.connector
 from mysql.connector import Error
-import os  # Importation pour lire les variables d'environnement
-
+import os
 
 def get_db_connection():
     try:
-        # Récupérer les variables d'environnement
-        host = os.environ.get("MYSQL_HOST")  # Hôte MySQL
-        user = os.environ.get("MYSQL_USER")  # Utilisateur MySQL
-        password = os.environ.get("MYSQL_PASSWORD")  # Mot de passe MySQL
-        database = os.environ.get("MYSQL_DATABASE")  # Base de données MySQL
-        port = os.environ.get("MYSQL_PORT")  # Port MySQL
-
-        # Vérifier que toutes les variables d'environnement sont définies
-        if None in [host, user, password, database, port]:
-            print("Erreur: Une ou plusieurs variables d'environnement ne sont pas définies.")
-            return None
-
-        # Conversion du port en entier
-        port = int(port)
-
-        # Connexion à la base de données
         connection = mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database,
-            port=port
+            host=os.environ.get("MYSQL_HOST", "junction.proxy.rlwy.net"),  # Hôte par défaut
+            port=int(os.environ.get("MYSQL_PORT", 10659)),  # Port par défaut
+            user=os.environ.get("MYSQL_USER", "root"),  # Utilisateur MySQL par défaut
+            password=os.environ.get("MYSQL_PASSWORD", "koNVTUXkFZcbxYpQUfxPvyWQdZJyFYrA"),  # Mot de passe MySQL par défaut
+            database=os.environ.get("MYSQL_DATABASE", "railway")  # Nom de la base de données par défaut
         )
-
         if connection.is_connected():
+            print("Connexion réussie à la base de données")
             return connection
     except Error as e:
-        print("Erreur lors de la connexion:", e)
+        print("Erreur lors de la connexion à la base de données:", e)
         return None
